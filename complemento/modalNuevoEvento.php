4bfdd72b4aed8bed15340fd2299e47ddce432ca3
coderp <codedemonscm@gmail.com>
2023-11-13 00:26:54 -0600

    agregando modificaciones a los eventos

--- complemento/modalNuevoEvento.php
+++ complemento/modalNuevoEvento.php
@@ -15,43 +15,41 @@
 			</div>
 		</div>
     <div class="form-group">
-      <label for="fecha_inicio" class="col-sm-12 control-label">Fecha Inicio</label>
+      <label for="fecha_inicio" class="col-sm-12 control-label">Fecha</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" name="fecha_inicio" id="fecha_inicio" placeholder="Fecha Inicio">
+        <input type="text" class="form-control" name="fecha_inicio" id="fecha_inicio" placeholder="Fechagi ">
       </div>
     </div>
     <div class="form-group">
-      <label for="fecha_fin" class="col-sm-12 control-label">Fecha Final</label>
+      <label for="fecha_fin" class="col-sm-12 control-label">Hora</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" name="fecha_fin" id="fecha_fin" placeholder="Fecha Final">
+        <input type="text" class="form-control" name="fecha_fin" id="fecha_fin" placeholder="Hora">
       </div>
     </div>
-
+    <div class="form-group">
+      <label for="fecha_fin" class="col-sm-12 control-label">Precio</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" name="fecha_fin" id="fecha_fin" placeholder="0.0">
+      </div>
+    </div>
   <div class="col-md-12" id="grupoRadio">
   
-  <input type="radio" name="color_evento" id="orange" value="#FF5722" checked>
-  <label for="orange" class="circu" style="background-color: #FF5722;"> </label>
-
-  <input type="radio" name="color_evento" id="amber" value="#FFC107">  
-  <label for="amber" class="circu" style="background-color: #FFC107;"> </label>
-
-  <input type="radio" name="color_evento" id="lime" value="#8BC34A">  
-  <label for="lime" class="circu" style="background-color: #8BC34A;"> </label>
-
-  <input type="radio" name="color_evento" id="teal" value="#009688">  
-  <label for="teal" class="circu" style="background-color: #009688;"> </label>
-
   <input type="radio" name="color_evento" id="blue" value="#2196F3">  
   <label for="blue" class="circu" style="background-color: #2196F3;"> </label>
 
+  <!---
   <input type="radio" name="color_evento" id="indigo" value="#9c27b0">  
   <label for="indigo" class="circu" style="background-color: #9c27b0;"> </label>
-
+  -->
 </div>
 		
 	   <div class="modal-footer">
-      	<button type="submit" class="btn btn-success">Guardar Evento</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Salir</button>
+      	<button type="submit" class="btn btn-success">          
+        <i class="material-icons">save</i>
+        </button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">
+        <i class="material-icons">close</i>
+        </button>
     	</div>
 	</form>
       
